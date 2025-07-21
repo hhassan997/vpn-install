1badf17dd6ee9a22df63b800eb2943b7748b5b74
hhassan997 <125778223+hhassan997@users.noreply.github.com>
2025-07-21 13:30:15 +0200

    Update install.sh

--- ipsec/install.sh
+++ ipsec/install.sh
@@ -59,7 +59,7 @@ $DIR/adduser.sh
 echo
 echo "Starting strongSwan and xl2tp..."
 service xl2tpd restart
-service strongswan restart
+service strongswan-starter restart
 
 echo
 echo "Installation script has been completed!"
